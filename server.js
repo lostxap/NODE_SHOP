169dde6c71052b6bdc791ed08a2ee7e056212328
lostxap <lostxap@gmail.com>
2020-08-22 12:36:57 +0300

    Simisdungeon

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const carti = require('./carti.json');
+const fs = require('fs');
 const app = express();
+const path = require('path')
 
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req,res) => {
+app.use(express.static(__dirname + '/simi_blog'));
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, '/simi_blog', 'index.html'))
+})
+
+
+app.get('/stories', (req,res) => {
     res.render('index' , {
-        title: 'MAGAZIN ONLINE',
+        title: "SIMI'S DUNGEON",
         carti: carti.profiles
     });
 });
@@ -21,10 +30,13 @@ app.get('/profile', (req, res) => {
     });
   });
 
-const server = app.listen(3000, (err) => {
+
+const server = app.listen(4000, (err) => {
     if (err){
         console.log(err);
     }else{
         console.log(`Server running on ${server.address().port}`);
     }
 });
+
+
